15567fc09264e1f4a80ffaef372ce926f66a430c
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2022-11-15 19:52:34 -0300

    feature: criacao de capitulo

--- app/src/main/java/com/fiap/befic/activity/CreateChapterActivity.kt
+++ app/src/main/java/com/fiap/befic/activity/CreateChapterActivity.kt
@@ -4,6 +4,7 @@ import BeficBackendFactory
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
@@ -107,12 +108,17 @@ class CreateChapterActivity : AppCompatActivity() {
                         Toast.LENGTH_SHORT
                     ).show()
 
-                    val i = Intent(context, ChapterReadActivity::class.java)
-                    i.putExtra("USER_ID", UserInfoUtils.userId);
-                    i.putExtra("STORY_ID", it.historiaId);
-                    i.putExtra("STORY_NAME", storyName);
-                    i.putExtra("CHAPTER_NUMBER", it.numero);
-                    startActivity(i)
+                    Handler()
+                        .postDelayed(
+                            Runnable {
+                                val i = Intent(context, ChapterReadActivity::class.java)
+                                i.putExtra("USER_ID", UserInfoUtils.userId);
+                                i.putExtra("STORY_ID", it.historiaId);
+                                i.putExtra("STORY_NAME", storyName);
+                                i.putExtra("CHAPTER_NUMBER", it.numero);
+                                startActivity(i)
+                            }, 2000
+                        )
 
                 }
             }
